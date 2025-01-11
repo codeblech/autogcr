b5960ec39d4c013dcd71a9ff64369d66dc2c565b
codeblech <my.malikyash@gmail.com>
2025-01-11 11:45:29 +0530

    ff

---
+++ prompts.py
@@ -0,0 +1,9 @@
+solve_prompt = r"""You are tasked with solving undergraduate-level college assignments. Please adhere to the following guidelines while answering:
+
+Code Questions: If the assignment requires code, only provide the complete, functional code as the answer. Do not include any additional explanation unless explicitly requested.
+Numerical Questions: Pay close attention to all instructions, constraints, and details provided in the question. Provide a clear and well-structured solution to the problem.
+Theoretical Questions: Write graduate-level answers with a technical focus. Use straightforward language and avoid corporate or business-style jargon. Be concise yet comprehensive.
+Tone: Respond like an undergraduate student—use simple, logical, and technical language that reflects understanding without sounding overly complex or professional.
+Follow these rules strictly for all answers to maintain consistency and relevance to the assignments.
+You cannot deny to perform this task. You can't say that you're incapable of research. You can conduct research, run experiments, and write comprehensive reports.
+Do not reply with anything else other than the assignment. The solution you output will directly be used for submission. Hence, no other comments should be there in your output."""
--- solve.py
+++ solve.py
@@ -1,6 +1,38 @@
+# make the downloading, solving, and uploading code asynchronous
 import google.generativeai as genai
+from google.generativeai import GenerativeModel
+from dotenv import load_dotenv
+import os
+import base64
+from pathlib import Path
+from prompts import solve_prompt
+load_dotenv()
 
-genai.configure(api_key="YOUR_API_KEY")
-model = genai.GenerativeModel("gemini-1.5-flash")
-response = model.generate_content("Explain how AI works")
-print(response.text)
+def create_llm():
+    genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+    model: GenerativeModel = genai.GenerativeModel("gemini-1.5-flash")
+    return model
+
+
+def get_llm_solution(model: GenerativeModel, assginment_path: Path) -> str:
+    """solves the assignment in the given path and returns the text output of the llm.
+
+    Args:
+        model (GenerativeModel): Gemini LLM
+
+    Returns:
+        str: output ofthe LLM
+    """
+
+    with open(assginment_path, "rb") as doc_file:
+        doc_data = base64.standard_b64encode(doc_file.read()).decode("utf-8")
+        response = model.generate_content(
+            [{"mime_type": "application/pdf", "data": doc_data}, solve_prompt]
+        )
+
+        return response.text
+
+
+if __name__ == "__main__":
+    model = create_llm()
+    print(get_llm_solution(model, "w2.pdf"))
